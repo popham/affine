a6a5acf688bc9292616fe8f0f2d54ba662188969
Tim Popham <popham@uw.edu>
2014-07-07 11:11:11 -0700

    Fixed exports

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
-define(['./lib/affine2'], function (affine2) {
+define(['./lib/2d/index'], function (affine2) {
     return {
         affine2 : affine2
     };
