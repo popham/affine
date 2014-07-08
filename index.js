e937d99ccfa41af8cf670493bd725045a5ae3f18
Tim Popham <popham@uw.edu>
2014-07-07 17:36:42 -0700

    UMD

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var load = define;
-if (typeof load !== 'function') { var load = require('amdefine')(module); }
-
-load(['./lib/2d/index'], function (affine2) {
-    return {
-        affine2 : affine2
-    };
-});
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['exports', './lib/2d/index'], factory);
+    } else if (typeof exports === 'object') {
+        factory(exports, require('./lib/2d/index'));
+    }
+}(this, function (exports, affine2) {
+    exports.affine2 = affine2;
+}));
